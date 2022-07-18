721aa2fd309c0cb2339c3d3343b964bd7a08da95
megafatayat <P041121007@bussan.co.id>
2022-07-18 09:13:41 +0700

    init project

---
+++ testing/src/test/java/com/baf/testing/TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baf.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
